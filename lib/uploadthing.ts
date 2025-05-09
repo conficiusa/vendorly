48bdcba75d2bdf43bf9b7a639cb8c78549c1f784
conficiusa <addawebadua@gmail.com>
2025-05-09 10:51:54 +0000

    refactor: remove unused UTApi import and instance from uploadthing.ts

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,7 +1,5 @@
 import { UploadRouter } from "@/app/api/uploadthing/core";
 import { generateReactHelpers } from "@uploadthing/react";
-import { UTApi } from "uploadthing/server";
 
-export const utapi = new UTApi({});
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<UploadRouter>();
