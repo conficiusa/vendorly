51eac1d88369a239174b785ffd9045b3fa8ddb30
conficiusa <addawebadua@gmail.com>
2025-06-04 10:57:05 +0000

    fix

--- lib/queries/user/cart.ts
+++ lib/queries/user/cart.ts
@@ -6,12 +6,18 @@ import { unstable_cache as cache } from "next/cache";
 export const fetchCartCount = cache(
   async (userId: string) => {
     const sessionId = await getSessionId();
-
     const where: Prisma.CartWhereInput = {
       OR: [{ userId }, { sessionId: sessionId }],
     };
-    const cartCount = await prisma.cart.count({
+    const cart = await prisma.cart.findFirst({
       where,
+      select: {
+        id: true,
+      },
+    });
+
+    const cartCount = await prisma.cartItem.count({
+      where: { cartId: cart?.id },
     });
     return cartCount;
   },
